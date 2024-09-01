f5a3090a06e6090d7c75fa96df71b2a9b75fb90c
ahmednaseerhussain <ahmednaseerhussain@gmail.com>
2024-09-01 06:12:40 +0500

    cors try 1

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express');
-const cors = require('cors'); // Import the cors package
+const cors = require('cors');
 const app = express();
 const db = require('./db.json');
 
-app.use(cors()); // Use the cors middleware
+// CORS Middleware ko use karte hain
+app.use(cors());
 
 app.get('/api/db', (req, res) => {
   res.status(200).json(db);
+});
+
+// Add other routes if necessary
+app.get('/api/db/category', (req, res) => {
+  res.status(200).json(db.category);
+});
+
+app.get('/api/db/banner', (req, res) => {
+  res.status(200).json(db.banner);
 });
 
 const port = process.env.PORT || 8000;
