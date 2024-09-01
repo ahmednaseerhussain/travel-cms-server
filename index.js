b5843b01dd8df41e9c3c86aadfe1efd9892e4b5a
ahmednaseerhussain <ahmednaseerhussain@gmail.com>
2024-09-01 06:04:18 +0500

    remove node module

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const cors = require('cors'); // Import the cors package
 const app = express();
 const db = require('./db.json');
+
+app.use(cors()); // Use the cors middleware
 
 app.get('/api/db', (req, res) => {
   res.status(200).json(db);
 });
 
-// const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
-  console.log(`Server is running on port`);
+  console.log(`Server is running on port ${port}`);
 });
